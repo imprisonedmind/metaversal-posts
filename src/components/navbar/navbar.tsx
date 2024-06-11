9d02707ab1afaa2e6caea474573958fef0c5d281
Luke <luke.stephens@trintel.co.za>
2024-06-11 20:57:22 +0200

    add fake user profile

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@
 import { NavbarITem } from "@/components/navbar/navbarITem";
 import { useParams, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { RiAdminFill } from "react-icons/ri";
 
 export default function Navbar() {
   const { users, id } = useParams();
@@ -32,14 +33,22 @@ export default function Navbar() {
     <div
       className={`
         ${scrolled && "top-0 drop-shadow-lg md:top-2 md:rounded-md"}
-        sticky top-0 z-50 border-b border-neutral-100 bg-neutral-50 p-4 transition 
-        duration-150 ease-in-out md:mt-4
+        sticky top-0 z-50 flex items-center justify-between border-b border-neutral-100 
+        bg-neutral-50 p-4 transition duration-150 ease-in-out md:mt-4
       `}
     >
       <ul className={"flex flex-row "}>
         <NavbarITem title={"Posts"} active={pathname == "/"} />
         <NavbarITem title={"Users"} active={users != null} />
       </ul>
+      <div
+        className={`
+        flex cursor-pointer items-center justify-center rounded-full border 
+        border-neutral-50 bg-neutral-950 p-2 shadow-md
+      `}
+      >
+        <RiAdminFill className={"h-4 w-4 text-neutral-50"} />
+      </div>
     </div>
   );
 }
